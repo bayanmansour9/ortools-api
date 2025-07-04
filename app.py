0a8edafdb2409748bdce6a545821415a5068e2fa
Bayan Mansour <bayanmansour66@gmail.com>
2025-07-04 14:41:48 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/optimize', methods=['POST'])
+def optimize():
+    data = request.get_json()
+    locations = data.get('locations', [])
+
+    # رد تجريبي (بدون OR-Tools حالياً)
+    return jsonify({
+        'received_locations': locations,
+        'route': list(range(len(locations)))
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
